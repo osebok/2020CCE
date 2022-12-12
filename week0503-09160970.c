564680418fe90ac3e3bf87f06e061046c66aef80
osebok <79676858+osebok@users.noreply.github.com>
2022-12-13 02:10:50 +0800

    Add files via upload

---
+++ week0503-09160970.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    ///int a=10;
+    ///while(a>0){
+     ///   printf("a¬O%i, a>0\n",a);
+
+       /// a--;
+    ///}
+    for(int a=3;a>0;a--){
+        printf("a¬O%d,a>0\n",a);
+    }
+}
