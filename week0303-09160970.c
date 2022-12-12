e1d098e678be6e1edace73674dde7c4161ab0c74
osebok <79676858+osebok@users.noreply.github.com>
2022-12-13 01:38:16 +0800

    Add files via upload

---
+++ week0303-09160970.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("請輸入1個數字n: ");
+    int n;
+    scanf("%d",&n);
+
+    if(n>10) printf("n>10");
+    if(n<10) printf("n<10");
+    if(n==10) printf("n10");
+}
